46b98963b305a66a2d155451afbe22678d4c1cea
Renan Del Puppo Furtado <renan.puppo@gmail.com>
2020-08-01 18:18:23 -0300

    Coding ExtensionFileInfos

--- src/main/java/br/com/pupposoft/trustly/connector/git/domains/ExtensionFileInfos.java
+++ src/main/java/br/com/pupposoft/trustly/connector/git/domains/ExtensionFileInfos.java
@@ -1,6 +1,8 @@
 package br.com.pupposoft.trustly.connector.git.domains;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,8 +18,31 @@ public class ExtensionFileInfos {
 	public Long getAmountLines() {
 		return this.files
 			.stream()
-			.filter(fileInfo -> fileInfo.getLines() != null)
+			//.filter(fileInfo -> fileInfo.getLines() != null)
 			.mapToLong(fileInfo -> fileInfo.getLines())
 			.reduce(0, Long::sum);
+	}
+	
+	public BigDecimal getAmountSizeInBytes() {
+		
+		final List<FileSize> allFileSize = this.files
+		.stream()
+		.map(fileInfo -> fileInfo.getFileSize())
+		.collect(Collectors.toList());
+		
+		BigDecimal totalBytes = new BigDecimal("0.00");
+		for (final FileSize fileSize : allFileSize) {
+			if(Measurement.MBYTES.equals(fileSize.getMeasurement())) {
+				totalBytes = totalBytes.add(fileSize.getSize().multiply(new BigDecimal("1048576")));
+				
+			}else if(Measurement.BYTES.equals(fileSize.getMeasurement())) {
+				totalBytes = totalBytes.add(fileSize.getSize());
+				
+			}else {//Measurement.KBYTES
+				totalBytes = totalBytes.add(fileSize.getSize().multiply(new BigDecimal("1024")));
+			}
+		}
+		
+		return totalBytes;
 	}
 }
--- src/test/java/br/com/pupposoft/trustly/connector/git/domains/ExtensionFileInfosUnitTest.java
+++ src/test/java/br/com/pupposoft/trustly/connector/git/domains/ExtensionFileInfosUnitTest.java
@@ -2,6 +2,7 @@ package br.com.pupposoft.trustly.connector.git.domains;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,6 +42,51 @@ public class ExtensionFileInfosUnitTest {
 		final ExtensionFileInfos extensionFileInfos = new ExtensionFileInfos("txt", filesInfos);
 		
 		assertEquals(25L, extensionFileInfos.getAmountLines());
+	}
+	
+	@Test
+	public void getAmountBytes() {
+		final FileSize fileSizeA = Fixture.from(FileSize.class)
+				.gimme(FileInfoTemplate.ANY_SIZE, new Rule() {
+					{
+						add("size", new BigDecimal("7.41"));
+						add("measurement", Measurement.MBYTES);
+					}});
+		final FileInfo fileTxtA = Fixture.from(FileInfo.class)
+				.gimme(FileInfoTemplate.TXT, new Rule() {
+			{
+				add("fileSize", fileSizeA);
+			}});
+
+		final FileSize fileSizeB = Fixture.from(FileSize.class)
+				.gimme(FileInfoTemplate.ANY_SIZE, new Rule() {
+					{
+						add("size", new BigDecimal("2.42"));
+						add("measurement", Measurement.KBYTES);
+					}});
+		final FileInfo fileTxtB = Fixture.from(FileInfo.class)
+				.gimme(FileInfoTemplate.TXT, new Rule() {
+			{
+				add("fileSize", fileSizeB);
+			}});
+
+		final FileSize fileSizeC = Fixture.from(FileSize.class)
+				.gimme(FileInfoTemplate.ANY_SIZE, new Rule() {
+					{
+						add("size", new BigDecimal("82"));
+						add("measurement", Measurement.BYTES);
+					}});
+		final FileInfo fileTxtC = Fixture.from(FileInfo.class)
+				.gimme(FileInfoTemplate.TXT, new Rule() {
+			{
+				add("fileSize", fileSizeC);
+			}});
+		
+		//7769948.16B + 2478.08B + 82B = 7772508.24B
+		List<FileInfo> filesInfos = Arrays.asList(fileTxtA, fileTxtB, fileTxtC);
+		final ExtensionFileInfos extensionFileInfos = new ExtensionFileInfos("txt", filesInfos);
+		
+		assertEquals(new BigDecimal("7772508.24"), extensionFileInfos.getAmountSizeInBytes());
 	}
 	
 }
