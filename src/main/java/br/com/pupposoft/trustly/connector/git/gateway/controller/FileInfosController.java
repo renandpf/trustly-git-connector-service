8d7cf69d67e57a25cddd90ed05c5e1d3f461af03
Renan Del Puppo Furtado <renan.puppo@gmail.com>
2020-08-02 16:01:00 -0300

    Coding FileInfosController

--- src/main/java/br/com/pupposoft/trustly/connector/git/gateway/controller/FileInfosController.java
+++ src/main/java/br/com/pupposoft/trustly/connector/git/gateway/controller/FileInfosController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pupposoft.trustly.connector.git.domains.ExtensionFileInfos;
 import br.com.pupposoft.trustly.connector.git.gateway.controller.json.ExtensionFileInfosJson;
+import br.com.pupposoft.trustly.connector.git.gateway.controller.json.ExtensionFileInfosRequestJson;
 import br.com.pupposoft.trustly.connector.git.usecases.GetFilesInfosOrquestrator;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,9 +29,9 @@ public class FileInfosController {
 	
 	@ResponseStatus(HttpStatus.OK)
 	@PostMapping
-	public List<ExtensionFileInfosJson> getExtensionFileInfos(){
-		//TODO: receber por parametro
-		final List<ExtensionFileInfos> extensionFileInfos = this.getFilesInfosOrquestrator.get("https://github.com/renandpf/trustly-git-connector-service");
+	public List<ExtensionFileInfosJson> getExtensionFileInfos(
+			final @RequestBody(required = true) ExtensionFileInfosRequestJson extensionFileInfosRequestJson){
+		final List<ExtensionFileInfos> extensionFileInfos = this.getFilesInfosOrquestrator.get(extensionFileInfosRequestJson.getRepositoryBaseUrl());
 		final List<ExtensionFileInfosJson> extensionFileInfosJson = 
 				extensionFileInfos
 				.stream()
---
+++ src/main/java/br/com/pupposoft/trustly/connector/git/gateway/controller/json/ExtensionFileInfosRequestJson.java
@@ -0,0 +1,10 @@
+package br.com.pupposoft.trustly.connector.git.gateway.controller.json;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ExtensionFileInfosRequestJson {
+	private String repositoryBaseUrl;
+}
