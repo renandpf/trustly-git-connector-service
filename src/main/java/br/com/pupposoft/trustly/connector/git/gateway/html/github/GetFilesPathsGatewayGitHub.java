68b1642227da5e0cb894d37cdcf2f80563545195
Renan Del Puppo Furtado <renan.puppo@gmail.com>
2020-08-02 09:02:52 -0300

    Coding GetFilesPathsGatewayGitHub

---
+++ src/main/java/br/com/pupposoft/trustly/connector/git/gateway/html/github/GetFilesPathsGatewayGitHub.java
@@ -0,0 +1,19 @@
+package br.com.pupposoft.trustly.connector.git.gateway.html.github;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import br.com.pupposoft.trustly.connector.git.gateway.html.GetFilesPathsGateway;
+
+@Component
+public class GetFilesPathsGatewayGitHub implements GetFilesPathsGateway {
+
+	@Override
+	public List<String> get(String urlAllFiles) {
+		// TODO Implementar
+		return null;
+	}
+
+
+}
